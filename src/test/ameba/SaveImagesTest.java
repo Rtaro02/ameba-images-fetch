f6033ed276fa1eed95224876bc482df0888578c5
Ryotaro Takeda <nmeeaiwxntwsoetnlelin@gmail.com>
2017-11-18 10:33:59 +0900

    add test

---
+++ src/test/ameba/SaveImagesTest.java
@@ -0,0 +1,62 @@
+package test.ameba;
+
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import static org.mockito.Mockito.*;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+
+import ameba.SaveImages;
+import httpclient.GetImage;
+import httpclient.JavaNetHttpClient;
+
+import static org.junit.Assert.*;
+import static org.mockito.Matchers.*;
+
+@RunWith(PowerMockRunner.class)
+@PrepareForTest({JavaNetHttpClient.class})
+public class SaveImagesTest {
+
+	@Before
+	public void setUp() throws Exception {
+	    //PowerMockito.spy(GetImage.class);
+	}
+	
+	@Test
+	public void fetchImagesTest() throws NoSuchMethodException, SecurityException, IllegalAccessException, IllegalArgumentException, InvocationTargetException{
+		// Define filed
+		String url = "";
+		String path = "";
+		List<String> list = new ArrayList<>();
+		// Add factors
+		list.add("aaa.jpg");
+		list.add("aaa.html");
+		// Mock get request
+        PowerMockito.mockStatic(JavaNetHttpClient.class);
+		when(JavaNetHttpClient.executeGet(any())).thenReturn(list);
+	    PowerMockito.doReturn(null).when(GetImage.class);  
+	    
+	    GetImage.getInstance();
+		//doNothing()
+		SaveImages saveImages = SaveImages.getInstance();
+
+		// Mocked private method
+		Method method = SaveImages.class.getDeclaredMethod("fetchImages", String.class, String.class);
+		method.setAccessible(true);
+		String actual = (String) method.invoke(saveImages, url, path);
+
+		assertEquals(actual,"a");
+	}
+}
